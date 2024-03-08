f3daa6e9310f0c46b63c54e39c1d61f22ad06297
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-07 20:34:38 -0800

    Modified endpoint to include sales/

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["purchase", "upload"]
+    const endpoints = ["sales/purchase", "sales/upload"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
