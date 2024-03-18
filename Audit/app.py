eee03629a40463d613d4dbc2e63a91559bfd0a2d
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-18 12:58:24 -0700

    changed back

--- Audit/app.py
+++ Audit/app.py
@@ -33,24 +33,34 @@ logger = logging.getLogger('basicLogger')
 
 def get_purchases(index):
     """ Get ticket purchase in History """
-    hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+    app_config["events"]["port"])
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
-    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    # Here we reset the offset on start so that we retrieve
+    # messages at the beginning of the message queue.
+    # To prevent the for loop from blocking, we set the timeout to
+    # 100ms. There is a risk that this loop never stops if the
+    # index is large and messages are constantly being received!
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+    consumer_timeout_ms=1000)
     logger.info("Retrieving BP at index %d" % index)
     current_index = 0
     try:
         for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+
             if current_index == index:
-                # Extract relevant information from the message
-                data = {
-                    "timestamp": msg.timestamp,
-                    "value": msg.value.decode('utf-8')  # Assuming message value is a string
-                }
-                return data, 200
+                return msg, 200
+            
             current_index += 1
+
             if current_index > index:
                 break
+    # Find the event at the index you want and
+    # return code 200
+    # i.e., return event, 200
     except:
         logger.error("No more messages found")
         logger.error("Could not find BP at index %d" % index)
@@ -58,24 +68,34 @@ def get_purchases(index):
 
 def get_uploads(index):
     """ Get ticket upload in History """
-    hostname = "%s:%d" % (app_config["events"]["hostname"], app_config["events"]["port"])
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+    app_config["events"]["port"])
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
-    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    # Here we reset the offset on start so that we retrieve
+    # messages at the beginning of the message queue.
+    # To prevent the for loop from blocking, we set the timeout to
+    # 100ms. There is a risk that this loop never stops if the
+    # index is large and messages are constantly being received!
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+    consumer_timeout_ms=1000)
     logger.info("Retrieving ticket upload at index %d" % index)
     current_index = 0
     try:
         for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+
             if current_index == index:
-                # Extract relevant information from the message
-                data = {
-                    "timestamp": msg.timestamp,
-                    "value": msg.value.decode('utf-8')  # Assuming message value is a string
-                }
-                return data, 200
+                return msg, 200
+            
             current_index += 1
+
             if current_index > index:
                 break
+    # Find the event at the index you want and
+    # return code 200
+    # i.e., return event, 200
     except:
         logger.error("No more messages found")
         logger.error("Could not find ticket upload at index %d" % index)
