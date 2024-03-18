3895cf750c49fd7f841d59f2feb2ee9cc554810d
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-18 09:15:25 -0700

    Modified Endpoint for lab9

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import '../App.css';
 
 export default function EndpointAudit(props) {
+    const [index, setIndex] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
@@ -13,6 +14,7 @@ export default function EndpointAudit(props) {
             .then((result)=>{
 				console.log("Received Audit Results for " + props.endpoint)
                 setLog(result);
+                setIndex(rand_val); // Set index upon successful response
                 setIsLoaded(true);
             },(error) =>{
                 setError(error)
@@ -32,9 +34,9 @@ export default function EndpointAudit(props) {
         
         return (
             <div>
-                <h3>{props.endpoint}-{rand_val}</h3>
-                {JSON.stringify(log)}
+            <h3>{props.endpoint}-{index}</h3>
+            {JSON.stringify(log)}
             </div>
-        )
+            )
     }
 }
