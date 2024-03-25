6bddb848910c10a1a305f0d288a615e067de1660
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-25 10:11:58 -0700

    modified code

--- event_log/event_logs.py
+++ event_log/event_logs.py
@@ -5,15 +5,15 @@ class EventLogs(Base):
     """ Processing Event Log Messages """
     __tablename__ = "event_log"
 
-    id = Column(Integer, primary_key=True)
+    event_id = Column(Integer, primary_key=True)
     message = Column(String, nullable=False)
     message_code = Column(String, nullable=False)
     date_time = Column(DateTime, nullable=False)
 
-    def __init__(self, id, message, message_code,
+    def __init__(self, event_id, message, message_code,
                  date_time):
         """ Initializes a processing statistics object """
-        self.id = id
+        self.event_id = event_id
         self.message = message
         self.message_code = message_code
         self.date_time = date_time
@@ -21,7 +21,7 @@ class EventLogs(Base):
     def to_dict(self):
         """ Dictionary Representation of statistics """
         stats_dict = {
-            'id': self.id,
+            'event_id': self.event_id,
             'message': self.message,
             'message_code': self.message_code,
             'date_time': self.date_time.strftime("%Y-%m-%dT%H:%M:%S")
