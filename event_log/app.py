86b4548bee316a0510edcdd984e82563c36650dd
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-25 13:24:17 -0700

    made changes

--- event_log/app.py
+++ event_log/app.py
@@ -116,12 +116,6 @@ def process_messages():
         message_code = msg["message_code"]
         message_content = msg["message"]
 
-    for msg in consumer:
-        msg_str = msg.value.decode('utf-8')
-        msg = json.loads(msg_str)
-        logger.info("Message: %s" % msg)
-        payload = msg["payload"]
-
         session = DB_SESSION()
         event_log = EventLogs(message=message_content,message_code=message_code)
     
