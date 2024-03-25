5d1885fdf6e216b8dd637f5c8e64fe40ca29532b
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-25 09:57:54 -0700

    fixed typo:

--- event_log/create_table.py
+++ event_log/create_table.py
@@ -7,7 +7,7 @@ c.execute('''
     CREATE TABLE event_log (
         event_id INTEGER PRIMARY KEY ASC,
         message TEXT NOT NULL,
-        message_code TEXT NO NULL,
+        message_code TEXT NOT NULL,
         date_time VARCHAR(100) NOT NULL
     )
 ''')
