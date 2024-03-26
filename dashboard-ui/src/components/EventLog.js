c393b11cfc84e36b67a3f32a6c8bd97af88083e2
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-26 12:52:44 -0700

    modified code

--- dashboard-ui/src/components/EventLog.js
+++ dashboard-ui/src/components/EventLog.js
@@ -1,48 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 export default function AppEvents() {
     const [isLoaded, setIsLoaded] = useState(false);
     const [stats, setStats] = useState({});
-    const [error, setError] = useState(null)
+    const [error, setError] = useState(null);
 
-	const getEvents = () => {
-	
+    const getEvents = () => {
         fetch(`http://acit-3855-kafka.westus3.cloudapp.azure.com:8120/event_stats`)
             .then(res => res.json())
-            .then((result)=>{
-				console.log("Received events")
-                setStats(result);
-                setIsLoaded(true);
-            },(error) =>{
-                setError(error)
-                setIsLoaded(true);
-            })
-    }
-    useEffect(() => {
-		const interval = setInterval(() => getEvents(), 4000); // Update every 2 seconds
-		return() => clearInterval(interval);
-    }, [getEvents]);
+            .then(
+                (result) => {
+                    console.log("Received events", result);
+                    setStats(result);
+                    setIsLoaded(true);
+                },
+                (error) => {
+                    setError(error);
+                    setIsLoaded(true);
+                }
+            );
+    };
 
-    if (error){
-        return (<div className={"error"}>Error found when fetching from API</div>)
-    } else if (isLoaded === false){
-        return(<div>Loading...</div>)
-    } else if (isLoaded === true){
-        return(
+    useEffect(() => {
+        const interval = setInterval(getEvents, 4000); // Update every 4 seconds
+        return () => clearInterval(interval);
+    }, []);
+
+    if (error) {
+        return (<div className={"error"}>Error found when fetching from API</div>);
+    } else if (!isLoaded) {
+        return (<div>Loading...</div>);
+    } else {
+        return (
             <div>
                 <table className={"EventTable"}>
-					<tbody>
-						<tr>
-							<td># Event 0001: {stats["0001"]}</td>
-							<td># Event 0002 {stats[ "0002"]}</td>
-                            <td># Event 0002 {stats[ "0003"]}</td>
-                            <td># Event 0004 {stats[ "0004"]}</td>
-						</tr>
-					</tbody>
+                    <tbody>
+                        <tr>
+                            <td># Event 0001: {stats["0001"]}</td>
+                            <td># Event 0002: {stats["0002"]}</td>
+                            <td># Event 0003: {stats["0003"]}</td>
+                            <td># Event 0004: {stats["0004"]}</td>
+                        </tr>
+                    </tbody>
                 </table>
-
             </div>
-        )
+        );
     }
 }
