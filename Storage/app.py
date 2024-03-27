834bdf63645bde54c1801e1ab1e80f4886122e19
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-27 01:32:14 -0700

    modified code

--- Storage/app.py
+++ Storage/app.py
@@ -31,8 +31,6 @@ with open('log_conf.yml', 'r') as f:
 
 
 logger = logging.getLogger('basicLogger')
-sleepy_time = app_config['sleepy_time']["sleep_in_sec"]
-max_retries = app_config["retries"]['retry_count']
 
 DB_ENGINE = create_engine(
     f'mysql+pymysql://{app_config["datastore"]["user"]}:{app_config["datastore"]["password"]}'
@@ -50,9 +48,6 @@ DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
 MAX_EVENTS= 5
 EVENT_FILE = "events.json"
-
-
-current_retry_count2 = 0
 
 
 def get_purchases(start_timestamp, end_timestamp):
@@ -93,9 +88,10 @@ def get_uploads(start_timestamp, end_timestamp):
 
 def process_messages():
     """ Process event messages """
-    # Declare current_retry_count here
-    current_retry_count = 0
-    
+
+    hostname = "%s:%d" % (app_config["event_log"]["hostname"],app_config["events"]["port"])
+
+    current_retry_count = 0 
     while current_retry_count < app_config["retries"]['retry_count']:
         logger.info(f"Connecting to Kafka. Current retry count: {current_retry_count}")
         try:    
@@ -113,30 +109,27 @@ def process_messages():
             logger.error("Connection failed")
             time.sleep(sleepy_time)
             current_retry_count += 1
-
+           
     logger.debug("Start of process_messages function")
     hostname = "%s:%d" % (app_config["events"]["hostname"],app_config["events"]["port"])
+    sleepy_time = app_config['sleepy_time']["sleep_in_sec"]
+    max_retries = app_config["retries"]['retry_count']
 
-    
-    # Create a consume on a consumer group, that only reads new messages
-    # (uncommitted messages) when the service re-starts (i.e., it doesn't
-    # read all the old messages from the history in the message queue).
-    # This is blocking - it will wait for a new message
-    current_retry_count = 0 
+    current_retry_count2 = 0 
     
     while current_retry_count2 < app_config["retries"]['retry_count']:
-        try:
+        logger.info(f"Connecting to Kafka. Current retry count: {current_retry_count2}")
+        try:    
             client = KafkaClient(hosts=hostname)
             topic = client.topics[str.encode(app_config["events"]["topic"])]
+            logger.info("Connected!")
+            break #yahoo 
         except:
             logger.error("Connection failed")
             time.sleep(sleepy_time)
-            logger.info(f"Connecting to Kafka. Current retry count: {current_retry_count2}")
-            currnet_retry_count2 += 1
-            if current_retry_count2 == max_retries:
-                    logger.error("Failed to connect to Kafka after %d retries. Exiting.", max_retries)
-                    return
+            current_retry_count2 += 1
         
+
     consumer = topic.get_simple_consumer(consumer_group=b'event_group', reset_offset_on_start=False, auto_offset_reset=OffsetType.LATEST)
 
     for msg in consumer:
@@ -185,18 +178,19 @@ def process_messages():
         # Store the event2 (i.e., the payload) to the DB
         # Commit the new message as being read
         consumer.commit_offsets()
-shabadoo = 0
+
+
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml",
             strict_validation=True,
             validate_responses=True)
-
+     
 
 if __name__ == "__main__":
     t1 = Thread(target=process_messages)
     
-    t1.setDaemon(True)
+    t1.daemon = True 
 
     t1.start()
 
