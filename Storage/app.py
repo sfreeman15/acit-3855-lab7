b021d899db15f6932075262f479bba30618c8b16
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-27 16:18:38 -0700

    modified code

--- Storage/app.py
+++ Storage/app.py
@@ -24,8 +24,8 @@ import os
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
-    app_conf_file = "/config/storage/app_conf.yml"
-    log_conf_file = "/config/storage/log_conf.yml"
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
 else:
     print("In Dev Environment")
     app_conf_file = "app_conf.yml"
