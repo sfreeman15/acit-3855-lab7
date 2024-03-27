769aa2bc31386fd9cf4836254cd473dc2729b288
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-27 14:54:10 -0700

    modified code

--- processing/app.py
+++ processing/app.py
@@ -142,10 +142,22 @@ def populate_stats():
         msg_str = json.dumps(msg)
         producer2.produce(msg_str.encode('utf-8'))
 
-    
-    max_value_p = most_recent_statistic.max_tp_readings
+        max_value_p = most_recent_statistic.max_tp_readings
     max_value_u = most_recent_statistic.max_tu_readings
     
+   # # Check if the length of purchase_data exceeds the current maximum TP readings
+   #  # and if the number of TP readings didn't increase
+   #  if len(purchase_data) > max_value_p:
+   #      # Update max_value_p with the new maximum TP readings
+   #      max_value_p = len(purchase_data)
+   #      # Update updated_purchase_val with the new maximum TP readings
+
+   #  # Check if the length of upload_data exceeds the current maximum TU readings
+   #  # and if the number of TU readings didn't increase
+   #  if len(upload_data) > max_value_u:
+   #      # Update max_value_u with the new maximum TU readings
+   #      max_value_u = len(upload_data)
+   #      # Update updated_upload_val with the new maximum TU readings
 
     for i in purchase_data:
          if max_value_p < i["price"]:
@@ -161,15 +173,20 @@ def populate_stats():
         for index in range(len(upload_data)):
                 logger.debug(f'Upload trace_id: {upload_data[index]["trace_id"]}')
             
+        # logger.debug(f'Updated Statistics Values - num_tp_readings: {most_recent_statistic.num_tp_readings}, num_tu_readings: {most_recent_statistic.num_tu_readings}, max_tp_readings: {most_recent_statistic.max_tp_readings}, max_tu_readings: {most_recent_statistic.max_tu_readings}, last_updated: {most_recent_statistic.last_updated}')
 
 
     logger.info("yes")
+    # logger.info(f"{purchase_data.status_code}")
     
 
     logger.info(f"Number of purchase events received: {len(purchase_data)}. Number of upload events received: {len(upload_data)}")
     stats = Stats(num_tp_readings=most_recent_statistic.num_tp_readings + len(purchase_data), num_tu_readings=most_recent_statistic.num_tu_readings + len(upload_data), max_tp_readings=max_value_p, max_tu_readings=max_value_u, last_updated=last_hour_datetime)
     
     
+    
+
+
 
     
     if stats:
@@ -185,8 +202,6 @@ def populate_stats():
     print(purchase_data)
     logger.info("Processing Period has ended.")
     
-
-
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True, timezone=timezone('America/Los_Angeles'))
