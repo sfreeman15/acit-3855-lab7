e1ddebe72de243cbf4ab920da294177bf01ea657
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-27 15:35:00 -0700

    modified code

--- processing/app.py
+++ processing/app.py
@@ -19,12 +19,29 @@ from pytz import timezone
 from pykafka import KafkaClient
 import time
 
-with open('app_conf.yml', 'r') as f:
+import os
+
+
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('log_conf.yml', 'r') as f:
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
+    logger = logging.getLogger('basicLogger')
+    logger.info("App Conf File: %s" % app_conf_file)
+    logger.info("Log Conf File: %s" % log_conf_file)
+
 
 sleepy_time = app_config['sleepy_time']["sleep_in_sec"]
 
