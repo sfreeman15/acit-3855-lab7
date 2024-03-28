4ef5a64491bdafd848dadc50eddadf0fd16e0660
Simon Freeman <sfreeman15@my.bcit.ca>
2024-03-27 20:23:12 -0700

    made changes

--- event_log/app.py
+++ event_log/app.py
@@ -29,15 +29,27 @@ from sqlalchemy import func
 import logging
 
 
-with open('app_conf.yml', 'r') as f:
-    app_config = yaml.safe_load(f.read())
-
-with open('log_conf.yml', 'r') as f:
-    log_config = yaml.safe_load(f.read())
-    logging.config.dictConfig(log_config)
-
-
+import os
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+print("In Test Environment")
+app_conf_file = "/config/app_conf.yml"
+log_conf_file = "/config/log_conf.yml"
+else:
+print("In Dev Environment")
+app_conf_file = "app_conf.yml"
+log_conf_file = "log_conf.yml"
+with open(app_conf_file, 'r') as f:
+app_config = yaml.safe_load(f.read())
+# External Logging Configuration
+with open(log_conf_file, 'r') as f:
+log_config = yaml.safe_load(f.read())
+logging.config.dictConfig(log_config)
 logger = logging.getLogger('basicLogger')
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
+
+
+
 logger.info(f'Connecting to DB.Hostname:"{app_config["datastore"]["filename"]}')
 
 database_path = "/app/event_log.sqlite"  # Update this with the correct path
